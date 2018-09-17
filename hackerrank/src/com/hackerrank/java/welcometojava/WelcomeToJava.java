85c70a278b1381e9cbbea8bd18bd9a68b41cdec7
Saydul Bashar <tamimnoone@gmail.com>
2018-09-17 17:34:18 +0100

    Added skeleton code for solution class and added test skeleton.

--- hackerrank/src/com/hackerrank/java/welcometojava/WelcomeToJava.java
+++ hackerrank/src/com/hackerrank/java/welcometojava/WelcomeToJava.java
@@ -1,4 +1,10 @@
 package com.hackerrank.java.welcometojava;
 
+/**
+ *  Solution for Hacker Rank problem https://www.hackerrank.com/challenges/welcome-to-java/problem
+ */
 public class WelcomeToJava {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
 }
